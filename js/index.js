387712f1e6ae2f21115ac615fc594e0f13fc139f
Diego Martinez <juandiego337@hotmail.com>
2021-10-05 14:15:31 -0500

    Operation multiplication is ok

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import {suma} from './operations.js'
+import {suma, restar, multiplicar} from './operations.js'
 
 let numberOne = document.querySelector("#numberOne");
 let numberTwo = document.querySelector("#numberTwo");
@@ -12,7 +12,11 @@ function generateCalculations(){
     let number1 = parseInt(numberOne.value);
     let number2 = parseInt(numberTwo.value);
 
-    alert(suma(number1,number2))
+    console.log("La suma es " + suma(number1,number2));
+
+    console.log("La resta es " + restar(number1,number2));
+
+    console.log("La multiplicacion es " + multiplicar(number1,number2))
 
 }
 
--- js/operations.js
+++ js/operations.js
@@ -1,2 +1,9 @@
 
 export const suma = (a,b) => a+b;
+
+export const restar = (a,b)=> a-b;
+
+export const multiplicar = (a,b)=>{
+    return new Array(b).fill(a).reduce(suma)
+}
+
